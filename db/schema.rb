4b79dec2430dce2670392fb4873552455a4aaa31
Kris Lapenskie <lapenskie@gmail.com>
2013-07-26 13:19:53 -0400

    Redo changes to bootstrap

--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130718222528) do
+ActiveRecord::Schema.define(version: 20130725185843) do
 
   create_table "tickets", force: true do |t|
     t.string   "name"
     t.string   "owner"
     t.string   "body"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "users", force: true do |t|
+    t.string   "username"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
